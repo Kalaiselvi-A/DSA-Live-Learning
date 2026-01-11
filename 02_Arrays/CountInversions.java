10a996fc0d7979f10a1f19100ec625a9d341f4d8
KALAISELVI A <121758407+Kalaiselvi-A@users.noreply.github.com>
2026-01-11 19:35:00 +0530

    Problem 40

---
+++ 02_Arrays/CountInversions.java
@@ -0,0 +1,49 @@
+public class CountInversions {
+
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 2, 1};
+        System.out.println("Inversion Count = " + mergeSort(arr, 0, arr.length - 1));
+    }
+
+    static int mergeSort(int[] arr, int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            count += mergeSort(arr, left, mid);
+            count += mergeSort(arr, mid + 1, right);
+            count += merge(arr, left, mid, right);
+        }
+        return count;
+    }
+
+    static int merge(int[] arr, int left, int mid, int right) {
+        int[] leftArr = new int[mid - left + 1];
+        int[] rightArr = new int[right - mid];
+
+        for (int i = 0; i < leftArr.length; i++)
+            leftArr[i] = arr[left + i];
+
+        for (int i = 0; i < rightArr.length; i++)
+            rightArr[i] = arr[mid + 1 + i];
+
+        int i = 0, j = 0, k = left, swaps = 0;
+
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+                swaps += (leftArr.length - i); // key line
+            }
+        }
+
+        while (i < leftArr.length)
+            arr[k++] = leftArr[i++];
+
+        while (j < rightArr.length)
+            arr[k++] = rightArr[j++];
+
+        return swaps;
+    }
+}
