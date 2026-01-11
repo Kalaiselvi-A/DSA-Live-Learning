2bd91a3c3e937386469120eff60ee10e7fa039f2
KALAISELVI A <121758407+Kalaiselvi-A@users.noreply.github.com>
2026-01-11 16:57:26 +0530

    Problem 12

---
+++ 02_Arrays/MaxMinSingleScan.java
@@ -0,0 +1,27 @@
+public class MaxMinSingleScan {
+
+    public static void main(String[] args) {
+
+        int[] arr = {6, 1, 8, 4};
+
+        int max = arr[0];
+        int min = arr[0];
+
+        /*
+         Single loop to find both max and min
+        */
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
