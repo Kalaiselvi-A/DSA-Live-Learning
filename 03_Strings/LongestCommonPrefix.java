ce88dee307d2bb009786524d5f204d1fa092a595
KALAISELVI A <121758407+Kalaiselvi-A@users.noreply.github.com>
2026-01-11 21:12:18 +0530

    Problem 16

---
+++ 03_Strings/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+        String[] strs = {"flower", "flow", "flight"};
+
+        if (strs.length == 0) {
+            System.out.println("");
+            return;
+        }
+
+        String prefix = strs[0]; // take first string as reference
+
+        // Compare prefix with each string
+        for (int i = 1; i < strs.length; i++) {
+
+            // Shorten prefix until it matches the start of strs[i]
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    System.out.println("");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("Longest Common Prefix = " + prefix);
+    }
+}
