5e0ec03a99a47c84901d85fec338f4a1f6621437
KALAISELVI A <121758407+Kalaiselvi-A@users.noreply.github.com>
2026-01-11 15:00:19 +0530

    Problem 1

--- 01_DSA_Basics/CheckAlgorithm.java
+++ 01_DSA_Basics/CheckAlgorithm.java
@@ -1,1 +1,22 @@
+public class CheckAlgorithm {
+
+    public static void main(String[] args) {
+
+        // These values simulate the properties of a process
+        boolean hasInput = true;       // Does it take input?
+        boolean hasOutput = true;      // Does it produce output?
+        boolean isFinite = true;       // Does it end?
+        boolean isUnambiguous = true;  // Are the steps clear?
+
+        /*
+         A valid algorithm must satisfy all four conditions.
+         We use logical AND (&&) to ensure all are true.
+        */
+        if (hasInput && hasOutput && isFinite && isUnambiguous) {
+            System.out.println("YES");   // It is an algorithm
+        } else {
+            System.out.println("NO");    // It is not an algorithm
+        }
+    }
+}
 
